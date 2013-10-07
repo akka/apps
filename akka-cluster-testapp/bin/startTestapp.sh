9abfc09a9cf92727d6aa5c5d5cab32304bf73cd8
Patrik Nordwall <patrik.nordwall@gmail.com>
2013-10-07 12:33:15 +0200

    test01

--- akka-cluster-testapp/bin/startTestapp.sh
+++ akka-cluster-testapp/bin/startTestapp.sh
@@ -10,13 +10,13 @@
 PORT="0"
 LOG_Q="testapp-`uuidgen`"
 
-HOST="$HOSTNAME"
+HOST="$(hostname)"
 
 SEED1="n0001:2552"
 SEED2="n0002:2552"
 SEED3="n0003:2552"
 
-LOG_DIR="/home/patrik/akka-logs"
+LOG_DIR="akka-logs"
 
 AKKA_HOME="$(cd "$(cd "$(dirname "$0")"; pwd -P)"/..; pwd)"
 AKKA_CLASSPATH="$AKKA_HOME/config:$AKKA_HOME/lib/*"
@@ -30,8 +30,8 @@ HOST_OPTS="-Dakka.remote.netty.tcp.hostname=$HOST -Dakka.cluster.seed-nodes.1=ak
 PORT_OPTS="-Dakka.remote.netty.tcp.port=$PORT"
 LOG_OPTS="-Dtestapp.log-dir=$LOG_DIR -Dtestapp.log-qualifier=$LOG_Q"
 DEBUG="-Dakka.loglevel=DEBUG -Dakka.log-dead-letters=1000 -Dakka.remote.log-received-messages=on -Dakka.remote.log-sent-messages=on"
-JAVA_OPTS="$MEM_OPTS $PRINT_GC_OPTS $JMX_OPTS $HOST_OPTS $PORT_OPTS $LOG_OPTS $DEBUG"
-JAVA_HOME="/usr"
+JAVA_OPTS="$MEM_OPTS $PRINT_GC_OPTS $JMX_OPTS $HOST_OPTS $PORT_OPTS $LOG_OPTS"
+JAVA_HOME="/opt/jdk1.7.0_40"
 
 mkdir -p $LOG_DIR
 
