6a388b9d104889f72a3a33880504aa0ba6bcacb7
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-06-13 00:28:00 +0900

    small cleanup with sbt instalation (was submodule, same things though)

---
+++ infra/site-cookbooks/sbt-manual/recipes/default.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook Name:: gce-logging-agent
+# Recipe:: default
+#
+# Copyright 2017, Lightbend
+#
+# All rights reserved - Do Not Redistribute
+#
+
+user "akka" do 
+  home '/home/akka'
+  shell '/bin/bash'
+  password 'akka'
+  action :create
+  manage_home true
+end
+
+template '/home/akka/root-application.conf' do
+  source 'root-application.conf.rb'
+end
+
+template '/home/akka/run-multinode-benchmark.sh' do 
+  source 'run-multinode-benchmark.sh.rb'
+end
+
+template '/home/akka/multi-node-test.hosts' do 
+  source 'multi-node-test.hosts.rb'
+end
+
+# set up mutual (naive, same key) trust between benchmark nodes
+cookbook_file '/home/akka/.ssh/authorized_keys' do
+  source 'akka-user_authorized_keys'
+end
+cookbook_file '/home/akka/.ssh/id_rsa' do
+  source 'akka-user_id_rsa'
+end
+cookbook_file '/home/akka/.ssh/id_rsa.pub' do
+  source 'akka-user_id_rsa.pub'
+end
+
+bash "clone akka" do
+  code <<-EOH
+    cd /home/akka
+    rm -rf akka
+    git clone https://github.com/akka/akka
+
+    chown -R akka.akka akka
+  EOH
+end
