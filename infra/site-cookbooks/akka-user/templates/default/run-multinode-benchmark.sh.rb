e3b10a44fe74ee59935f15c1d768b4c5206dedeb
Patrik Nordwall <patrik.nordwall@gmail.com>
2017-06-13 18:05:29 +0200

    update run-multinode-benchmark.sh
    * tested new script (and rewrote it)

--- infra/site-cookbooks/akka-user/templates/default/run-multinode-benchmark.sh.rb
+++ infra/site-cookbooks/akka-user/templates/default/run-multinode-benchmark.sh.rb
@@ -5,50 +5,59 @@
 # $1 - IP to execute scripts on
 prepare() {
   declare -r IP2="$1"
-  
-  rm -rf $AERON_DIR
-  ssh $IP2 "cd $PWD; rm -rf $AERON_DIR"
-  
-  mkdir -p /home/akka/aeron; cd /home/akka/aeron; wget -nc http://repo1.maven.org/maven2/io/aeron/aeron-all/1.2.5/aeron-all-1.2.5.jar
-  ssh $IP2 "mkdir -p /home/akka/aeron; cd /home/akka/aeron; wget -nc http://repo1.maven.org/maven2/io/aeron/aeron-all/1.2.5/aeron-all-1.2.5.jar"
-  
-  java -cp /home/akka/aeron/aeron-all-1.2.5.jar $MEDIA_DRIVER_ARGS io.aeron.driver.MediaDriver &
-  ssh $IP2 "java -cp /home/akka/aeron/aeron-all-1.2.5.jar $MEDIA_DRIVER_ARGS io.aeron.driver.MediaDriver" &
-  sleep 10
-  
-  java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.AeronStat type=[1-9] > $LOGS1/aeron-stat.txt &
-  ssh $IP2 "java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.AeronStat type=[1-9] > $LOGS2/aeron-stat.txt" &
-  
-  vmstat 1 > $LOGS1/vmstat.txt &
-  ssh $IP2 "cd $PWD; vmstat 1 > $LOGS2/vmstat.txt" &
+  declare -r LOGS2="$LOGS_BASE/$IP2"
+
+  ssh $IP2 "
+    cd $PWD
+
+    mkdir -p $LOGS2
+
+    rm -rf $AERON_DIR
+
+    mkdir -p /home/akka/aeron
+    cd /home/akka/aeron
+    wget -nc http://repo1.maven.org/maven2/io/aeron/aeron-all/1.2.5/aeron-all-1.2.5.jar
+
+    java -cp /home/akka/aeron/aeron-all-1.2.5.jar $MEDIA_DRIVER_ARGS io.aeron.driver.MediaDriver &
+    sleep 10
+    
+    java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.AeronStat type=[1-9] > $LOGS2/aeron-stat.txt &
+
+    vmstat 1 > $LOGS2/vmstat.txt &
+  " &
+
 }
 
 # $1 - IP to execute scripts on
 finish() {
   declare -r IP2="$1"
+  declare -r LOGS2="$LOGS_BASE/$IP2"
+
+  ssh $IP2 "
+    cd $PWD
   
-  java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.ErrorStat > $LOGS1/aeron-err.txt
-  ssh $IP2 "java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.ErrorStat > $LOGS2/aeron-err.txt"
+    java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.ErrorStat > $LOGS2/aeron-err.txt
+
+    du -h /dev/shm/aeron-akka > $LOGS2/aeron-disk-usage.txt
   
-  cp -R $AERON_DIR $LOGS1
-  ssh $IP2 "cd $PWD; cp -R $AERON_DIR $LOGS2"
+    cp -R $AERON_DIR $LOGS2
   
-  pkill -u akka vmstat
-  ssh $IP2 "cd $PWD; pkill -u akka vmstat"
+    pkill -u akka vmstat
   
-  pkill -u akka -f aeron
-  ssh $IP2 "cd $PWD; pkill -u akka -f aeron"
+    pkill -u akka -f aeron
+  "
   
-  scp -r $IP2:$LOGS2 $LOGS_BASE/
+  # avoid copy own files
+  if [ ! -d "$LOGS_BASE/$IP2" ]; then
+    scp -r $IP2:$LOGS2 $LOGS_BASE/
+  fi
 }
 # -------------- end of functions -----------------------------------------------------------------------------------------------
 
 
 declare -r LOGS_BASE="/home/akka/log/$(date '+20%y-%m-%d--%H%M')"
-
-for ip in $(cat /home/akka/multi-node-test.hosts); do
-  mkdir -p $LOGS_BASE/$ip
-done
+declare -r IP1="$(head -n 1 multi-node-test.hosts)"
+declare -r LOGS1="$LOGS_BASE/$IP1"
 
 declare -r AERON_DIR=/dev/shm/aeron-akka
 
@@ -62,10 +71,11 @@ declare -r COMMON_ARGS="-Dakka.test.LatencySpec.totalMessagesFactor=30 -Dakka.te
 #declare -r ARGS="-Dakka.remote.artery.advanced.inbound-lanes=1"
 declare -r ARGS="$1"
 
+echo "logs in $LOGS_BASE"
 mkdir -p $LOGS1
-ssh $IP2 "cd $PWD; mkdir -p $LOGS2"
 
 # description of test 
+cd /home/akka/akka
 echo "$1" > $LOGS_BASE/readme.txt
 git log -n 1 >> $LOGS_BASE/readme.txt
 
@@ -74,8 +84,12 @@ for ip in $(cat /home/akka/multi-node-test.hosts); do
   prepare $ip
 done
 
+sleep 10
+
 # run the tests ------------------------------------------
+echo "running test, log: $LOGS1/log.txt"
 cd /home/akka/akka
+cp /home/akka/multi-node-test.hosts /home/akka/akka/
 /home/akka/sbt $ARGS $COMMON_ARGS $MULTI_NODE_ARGS 'akka-remote-tests/test' > $LOGS1/log.txt
 cd /home/akka/
 
