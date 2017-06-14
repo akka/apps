e47abf2a5cdd7b8668154b26fd506f8fa8ab9657
Arnout Engelen <arnouten@bzzt.net>
2017-06-14 15:42:59 +0200

    Pub-sub benchmark initial scaffold
    Currently very naive: each publisher/subscriber is associated with
    1 topic and bursts out messages when the benchmark starts, then
    after a timeout the number of received messages is collected from
    the subscribers.
    
    To make this benchmark more useful, we should:
    * make publishers/subscribers use multiple topics
    * measure more interesting things (latency/throughput?)
    * produce a 'controlled' load instead of bursting all messages
    
    Known problem: sometimes the benchmark fails to initialize, probably
    some message gets lost somewhere?

---
+++ apps/src/main/scala/com/lightbend/akka/bench/pubsub/Publisher.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 Lightbend Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lightbend.akka.bench.pubsub
+
+import akka.actor._
+import akka.cluster.pubsub._
+
+object Publisher {
+  def props(topic: Int, coordinator: ActorRef): Props = props(topic.toString, coordinator)
+  def props(topic: String, coordinator: ActorRef): Props = Props(new Publisher(topic, coordinator))
+
+  case class Started(publisher: ActorRef)
+  case object Tick
+}
+class Publisher(topic: String, coordinator: ActorRef) extends Actor {
+  import Publisher._
+  import DistributedPubSubMediator.Publish
+
+  coordinator ! Started(self)
+
+  // activate the extension
+  val mediator = DistributedPubSub(context.system).mediator
+
+  override def receive = {
+    case Tick => mediator ! Publish(topic, Payload(42))
+  }
+}
