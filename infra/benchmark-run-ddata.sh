2440a44717fe4fe0e6d17f718e798e20d3956835
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-06-14 13:12:28 +0900

    script to run DData benchmark!

---
+++ infra/benchmark-run-ddata.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+declare -r CLAZZ=com.lightbend.akka.bench.ddata.DistributedDataBenchmark
+
+fix --verbose --concurrency 2 \
+  nodes_with_role:benchmark \
+  ssh:"cd /home/akka/apps; sudo -u akka /home/akka/sbt ';project apps; runMain $CLAZZ'"
