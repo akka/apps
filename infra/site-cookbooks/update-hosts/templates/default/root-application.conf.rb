c6bcf421c498f530376b16ce43d21baa256be922
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-06-14 11:39:46 +0900

    WIP on prepping for ddata

--- infra/site-cookbooks/update-hosts/templates/default/root-application.conf.rb
+++ infra/site-cookbooks/update-hosts/templates/default/root-application.conf.rb
@@ -3,10 +3,21 @@ akka {
   remote {
     netty.tcp {
       hostname = "<%= node['ipaddress-internal'] %>"
+      port = <%= node['akka']['port'] %>
     }
-  }
+    
+    artery {
+      # enabled = on this is up to the app to decide
+      canonical.hostname = "<%= node['ipaddress-internal'] %>"
+      canonical.port = <%= node['akka']['port'] %>
+    }
+  } 
 
   cluster {
-    seed-nodes = <%= node['akka']['seed_nodes'] %>
+    seed-nodes = [
+      <%= (node['akka']['seed_nodes'].each do |seed|
+         "akka://#{node['akka']['system_name']}/@#{seed}:#{node['akka']['port']}"
+      end).join(",") %>
+    ]
   }
 }
