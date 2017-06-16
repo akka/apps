e40a3306b2c9aa62fe54985e603c987b391926bb
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-06-16 18:18:27 +0900

    Update benchmark-run-sharding-latency.sh

--- infra/benchmark-run-sharding-latency.sh
+++ infra/benchmark-run-sharding-latency.sh
@@ -2,6 +2,6 @@
 
 declare -r CLAZZ="com.lightbend.akka.bench.sharding.ShardingLatencyApp"
 
-fix --verbose --concurrency 3 \
+fix --verbose --concurrency 1000 \
   nodes_with_role:benchmark-sharding \
   ssh:"cd /home/akka/apps; sudo -u akka /home/akka/sbt ';project sharding; runMain $CLAZZ'"
