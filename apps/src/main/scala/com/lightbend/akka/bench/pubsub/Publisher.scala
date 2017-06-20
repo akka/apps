882cc750cb9a83e636a3c848fee0ea21689b45ab
Arnout Engelen <arnouten@bzzt.net>
2017-06-20 15:47:16 +0200

    Don't overload artery with 'tick' messages

--- apps/src/main/scala/com/lightbend/akka/bench/pubsub/Publisher.scala
+++ apps/src/main/scala/com/lightbend/akka/bench/pubsub/Publisher.scala
@@ -16,31 +16,50 @@
 
 package com.lightbend.akka.bench.pubsub
 
+import scala.concurrent.duration._
 import akka.actor._
 import akka.cluster.pubsub._
+
+import scala.concurrent.ExecutionContext
 
 object Publisher {
   def props(topic: Int, coordinator: ActorRef): Props = props(topic.toString, coordinator)
   def props(topic: String, coordinator: ActorRef): Props = Props(new Publisher(topic, coordinator))
 
   case class Started(publisher: ActorRef)
+  case class Start(messagesToPublish: Int)
   case object Tick
 }
 class Publisher(topic: String, coordinator: ActorRef) extends Actor with ActorLogging {
   import Publisher._
   import DistributedPubSubMediator.Publish
 
+  implicit val ec: ExecutionContext = context.dispatcher
+
   override def preStart() = {
 //    log.info(s"Started publisher for $topic under $self")
     coordinator ! Started(self)
   }
 
-  override def postStop() = log.info(s"Publisher stopped")
+//  override def postStop() = log.info(s"Publisher stopped")
 
   // activate the extension
   val mediator = DistributedPubSub(context.system).mediator
 
   override def receive = {
-    case Tick => mediator ! Publish(topic, Payload(42))
+    case Start(messagesToPublish) =>
+        self ! Tick
+        context.become(ticking(context.system.scheduler.schedule(0.seconds, 10.millis, self, Tick), messagesToPublish))
+  }
+
+  def ticking(cancellable: Cancellable, ticksLeft: Int): Receive = {
+    case Tick =>
+      mediator ! Publish(topic, Payload(42))
+      if (ticksLeft == 1) {
+        cancellable.cancel()
+        context.stop(self)
+      } else {
+        context.become(ticking(cancellable, ticksLeft - 1))
+      }
   }
 }
