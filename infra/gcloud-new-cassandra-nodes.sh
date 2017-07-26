daf81b09af3dc75a470b6fb5a5c27e17954b9db9
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-07-26 18:42:34 +0900

    script to start the new cassandra cluster

--- infra/gcloud-new-cassandra-nodes.sh
+++ infra/gcloud-new-cassandra-nodes.sh
@@ -4,77 +4,53 @@
 
 # NOTE: watch out to keep \ the last character (no trailing space)
 
-gcloud compute --project "akka-gcp" \
-  instances create "akka-cassandra-001" \
-  --zone "europe-west1-b" \
-  --machine-type "n1-standard-4" \
-  --subnet "default" --maintenance-policy "MIGRATE" --service-account "7250250762-compute@developer.gserviceaccount.com" \
-  --scopes "https://www.googleapis.com/auth/cloud-platform" \
-  --image "ubuntu-1704-zesty-v20170413" \
-  --image-project "ubuntu-os-cloud" \
-  --boot-disk-size "20" --boot-disk-type "pd-ssd" \
-  --boot-disk-device-name "akka-cassandra-001"
+echo "" > ./cassandra-hosts
 
-gcloud compute --project "akka-gcp" \
-  instances create "akka-cassandra-002" \
-  --zone "europe-west1-b" \
-  --machine-type "n1-standard-4" \
-  --subnet "default" --maintenance-policy "MIGRATE" --service-account "7250250762-compute@developer.gserviceaccount.com" \
-  --scopes "https://www.googleapis.com/auth/cloud-platform" \
-  --image "ubuntu-1704-zesty-v20170413" \
-  --image-project "ubuntu-os-cloud" \
-  --boot-disk-size "20" --boot-disk-type "pd-ssd" \
-  --boot-disk-device-name "akka-cassandra-002"
+for node_number in 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+do
+
+  gcloud compute --project "akka-gcp" instances create "akka-cassandra-$node_number" \
+  --zone "us-central1-a" \
+  --machine-type "n1-standard-16" --subnet "default" \
+  --metadata 'startup-script=#! /bin/bash\u000a    cd /home/akka\u000a    echo \"Running all startup-scripts in $(pwd)...\" \u000a    for script in $(ls *startup-script*); do\u000a      echo \"Running $script\"\u000a      ./$script\u000a    done'\
+   --maintenance-policy "MIGRATE" \
+   --service-account "7250250762-compute@developer.gserviceaccount.com" \
+   --scopes "https://www.googleapis.com/auth/cloud-platform" \
+   --tags "cassandra" --image "ubuntu-1704-zesty-v20170413" \
+   --image-project "ubuntu-os-cloud" \
+   --boot-disk-size "20" --boot-disk-type "pd-ssd" \
+   --boot-disk-device-name "akka-cassandra-$node_number"
   
-gcloud compute --project "akka-gcp" \
-  instances create "akka-cassandra-003" \
-  --zone "europe-west1-b" \
-  --machine-type "n1-standard-4" \
-  --subnet "default" --maintenance-policy "MIGRATE" --service-account "7250250762-compute@developer.gserviceaccount.com" \
-  --scopes "https://www.googleapis.com/auth/cloud-platform" \
-  --image "ubuntu-1704-zesty-v20170413" \
-  --image-project "ubuntu-os-cloud" \
-  --boot-disk-size "20" --boot-disk-type "pd-ssd" \
-  --boot-disk-device-name "akka-cassandra-003"
+  
+  # get all IP
+  
+  declare -r cassandra_internal_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-01" | head -n1 | awk '{ print $4 }')
+  declare -r cassandra_external_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-01" | head -n1 | awk '{ print $5 }')
+  
+  echo ${cassandra_external_ip} >> ./cassandra-hosts
+  
+  # prepare node descriptions
+  cat nodes/cassandra.json.template |
+    sed "s/NAME/$cassandra_external_ip/g" |   
+    sed "s/CASSANDRA_SEED_IP/$cassandra_internal_ip/g" |  
+    sed "s/EXTERNAL_IP/$cassandra_external_ip/g" | 
+    sed "s/INTERNAL_IP/$cassandra_internal_ip/g" > nodes/cassandra-${node_number}.json
+    
+  echo "Created nodes/cassandra-${node_number}.json..."
+  
+  # allow CQL access to nodes
+  echo "Allowing network connections..."
+  gcloud beta compute --project "akka-gcp" firewall-rules create "allow-cassandra" --allow tcp:9042 --direction "INGRESS" --priority "1000" --network "default" --source-ranges "0.0.0.0" --target-tags "cassandra"
 
-# get all IPs
-
-declare -r cassandra_001_internal_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-001" | head -n1 | awk '{ print $4 }')
-declare -r cassandra_001_external_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-001" | head -n1 | awk '{ print $5 }')
-
-declare -r cassandra_002_internal_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-002" | head -n1 | awk '{ print $4 }')
-declare -r cassandra_002_external_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-002" | head -n1 | awk '{ print $5 }')
-
-declare -r cassandra_003_internal_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-003" | head -n1 | awk '{ print $4 }')
-declare -r cassandra_003_external_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-003" | head -n1 | awk '{ print $5 }')
-
-# prepare node descriptions
-cat nodes/cassandra.json.template |
-  sed "s/NAME/$cassandra_001_external_ip/g" |   
-  sed "s/CASSANDRA_SEED_IP/$cassandra_001_internal_ip/g" |  
-  sed "s/EXTERNAL_IP/$cassandra_001_external_ip/g" | 
-  sed "s/INTERNAL_IP/$cassandra_001_internal_ip/g" > nodes/cassandra-001.json
-cat nodes/cassandra.template | 
-  sed "s/NAME/$cassandra_002_external_ip/g" | 
-  sed "s/CASSANDRA_SEED_IP/$cassandra_001_internal_ip/g" |  
-  sed "s/EXTERNAL_IP/$cassandra_002_external_ip/g" | 
-  sed "s/INTERNAL_IP/$cassandra_002_internal_ip/g" > nodes/cassandra-002.json
-cat nodes/cassandra.template | 
-  sed "s/NAME/$cassandra_003_external_ip/g" |    
-  sed "s/CASSANDRA_SEED_IP/$cassandra_001_internal_ip/g" |  
-  sed "s/EXTERNAL_IP/$cassandra_003_external_ip/g" | 
-  sed "s/INTERNAL_IP/$cassandra_003_internal_ip/g" > nodes/cassandra-003.json
-
-# allow CQL access to nodes
-gcloud beta compute --project "akka-gcp" firewall-rules create "allow-cassandra" --allow tcp:9042 --direction "INGRESS" --priority "1000" --network "default" --source-ranges "0.0.0.0" --target-tags "cassandra"
+done
 
 
-echo "------ UPDATE YOUR /etc/hosts -------"
 echo "# -----------------------------------"
 echo "# akka cluster on GCE"
-echo "$cassandra_001_external_ip cassandra-001"
-echo "$cassandra_002_external_ip cassandra-002"
-echo "$cassandra_003_external_ip cassandra-003"
+echo ""
+cat ./cassandra-hosts
+echo ""
+echo "------ UPDATE YOUR /etc/hosts -------"
 echo "# -----------------------------------"
 echo "      Press ENTER when ready...      "
 read
@@ -82,9 +58,10 @@ echo "# -----------------------------------"
 
 # chef all cassandra nodes
 echo "Deploying chef to all nodes"
-fix node:cassandra-001 deploy_chef -y
-fix node:cassandra-002 deploy_chef -y
-fix node:cassandra-003 deploy_chef -y
+for node_number in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+do 
+  fix node:cassandra-${node_number} deploy_chef -y
+done
 
 # cook all nodes!
 
