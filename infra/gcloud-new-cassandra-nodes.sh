b7f9befeb726c7b0ad7ede77daa34389cb144c6b
Konrad `ktoso` Malawski <ktoso@project13.pl>
2017-07-27 12:32:20 +0900

    cassandra 20 nodes

--- infra/gcloud-new-cassandra-nodes.sh
+++ infra/gcloud-new-cassandra-nodes.sh
@@ -4,7 +4,7 @@
 
 # NOTE: watch out to keep \ the last character (no trailing space)
 
-echo "" > ./cassandra-hosts
+rm ./cassandra-hosts
 
 for node_number in 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
 do
@@ -19,30 +19,31 @@ do
    --tags "cassandra" --image "ubuntu-1704-zesty-v20170413" \
    --image-project "ubuntu-os-cloud" \
    --boot-disk-size "20" --boot-disk-type "pd-ssd" \
-   --boot-disk-device-name "akka-cassandra-$node_number"
+   --boot-disk-device-name "akka-cassandra-$node_number" \
+   --no-address
   
   
   # get all IP
   
-  declare -r cassandra_internal_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-01" | head -n1 | awk '{ print $4 }')
-  declare -r cassandra_external_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-01" | head -n1 | awk '{ print $5 }')
+  cassandra_internal_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-$node_number" | head -n1 | awk '{ print $4 }')
+  cassandra_external_ip=$(gcloud --project="akka-gcp" compute instances list | grep "cassandra-$node_number" | head -n1 | awk '{ print $5 }')
   
   echo ${cassandra_external_ip} >> ./cassandra-hosts
   
   # prepare node descriptions
   cat nodes/cassandra.json.template |
     sed "s/NAME/$cassandra_external_ip/g" |   
-    sed "s/CASSANDRA_SEED_IP/$cassandra_internal_ip/g" |  
+    sed "s/CASSANDRA_SEED_IP/10.128.0.3/g" |  
     sed "s/EXTERNAL_IP/$cassandra_external_ip/g" | 
     sed "s/INTERNAL_IP/$cassandra_internal_ip/g" > nodes/cassandra-${node_number}.json
     
   echo "Created nodes/cassandra-${node_number}.json..."
   
-  # allow CQL access to nodes
-  echo "Allowing network connections..."
-  gcloud beta compute --project "akka-gcp" firewall-rules create "allow-cassandra" --allow tcp:9042 --direction "INGRESS" --priority "1000" --network "default" --source-ranges "0.0.0.0" --target-tags "cassandra"
-
 done
+
+## allow CQL access to nodes
+#echo "Allowing network connections..."
+#gcloud beta compute --project "akka-gcp" firewall-rules create "allow-cassandra" --allow tcp:9042 --direction "INGRESS" --priority "1000" --network "default" --source-ranges "0.0.0.0" --target-tags "cassandra"
 
 
 echo "# -----------------------------------"
