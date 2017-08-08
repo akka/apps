e72724731f28423d530aba3f4f32287e6361613d
Martynas Mickevičius <martynas@2m.lt>
2017-08-08 13:51:12 +0300

    10 nodes

--- infra/site-cookbooks/update-hosts/templates/default/run-multinode-benchmark.sh.rb
+++ infra/site-cookbooks/update-hosts/templates/default/run-multinode-benchmark.sh.rb
@@ -8,6 +8,7 @@ prepare() {
   declare -r LOGS2="$LOGS_BASE/$IP2"
 
   ssh $IP2 "
+    mkdir -p $PWD
     cd $PWD
 
     mkdir -p $LOGS2
@@ -16,15 +17,22 @@ prepare() {
 
     rm -f $MULTI_NODE_DIR/target/*.afr
 
+    # allow testing with buffers up to 64MB
+    echo 'net.core.rmem_max = 67108864' | sudo tee /etc/sysctl.conf
+    echo 'net.core.wmem_max = 67108864' | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -p
+
     cat /proc/cpuinfo > $LOGS2/cpuInfo.txt
 
     mkdir -p /home/akka/aeron
     cd /home/akka/aeron
-    wget -nc http://repo1.maven.org/maven2/io/aeron/aeron-all/1.2.5/aeron-all-1.2.5.jar
+    if [ ! -f ./aeron-all-1.2.5.jar ]; then
+      wget -nc http://repo1.maven.org/maven2/io/aeron/aeron-all/1.2.5/aeron-all-1.2.5.jar
+    fi
 
     java -cp /home/akka/aeron/aeron-all-1.2.5.jar $MEDIA_DRIVER_ARGS $ARGS io.aeron.driver.MediaDriver &
     sleep 10
-    
+
     java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.AeronStat type=[1-9] > $LOGS2/aeron-stat.txt &
 
     vmstat 1 > $LOGS2/vmstat.txt &
@@ -39,22 +47,22 @@ finish() {
 
   ssh $IP2 "
     cd $PWD
-  
+
     java -cp /home/akka/aeron/aeron-all-1.2.5.jar -Daeron.dir=$AERON_DIR io.aeron.samples.ErrorStat > $LOGS2/aeron-err.txt
 
     du -h /dev/shm/aeron-akka > $LOGS2/aeron-disk-usage.txt
-  
+
     cp -R $AERON_DIR $LOGS2
 
     cp $MULTI_NODE_DIR/target/*.afr $LOGS2/
-  
+
     pkill -u akka vmstat
-  
+
     pkill -u akka java
     sleep 5
     pkill -9 -u akka java
   "
-  
+
   # avoid copy own files
   if [ ! -d "$LOGS_BASE/$IP2" ]; then
     scp -r $IP2:$LOGS2 $LOGS_BASE/
@@ -73,7 +81,7 @@ declare -r AERON_DIR=/dev/shm/aeron-akka
 declare -r MEDIA_DRIVER_ARGS="-Daeron.threading.mode=SHARED_NETWORK -Xms1g -Xmx1g -XX:+UseCompressedOops -XX:MaxDirectMemorySize=256m -XX:ReservedCodeCacheSize=256m -XX:BiasedLockingStartupDelay=0 -Daeron.mtu.length=16384 -Daeron.socket.so_sndbuf=2097152 -Daeron.socket.so_rcvbuf=2097152 -Daeron.rcv.initial.window.length=2097152 -Daeron.term.buffer.sparse.file=false -Dagrona.disable.bounds.checks=true"
 
 declare -r MULTI_NODE_DIR="/home/akka/tmp-akka-multi-node"
-declare -r MULTI_NODE_ARGS="-Dakka.test.multi-node=true -Dakka.test.multi-node.targetDirName=$MULTI_NODE_DIR -Dakka.remote.artery.advanced.flight-recorder.destination=target -Dmultinode.Xms1024M -Dmultinode.Xmx1024M -Dmultinode.XX:+PrintGCDetails -Dmultinode.XX:+PrintGCTimeStamps -Dmultinode.XX:BiasedLockingStartupDelay=0 -Dmultinode.Daeron.mtu.length=16384 -Dmultinode.Daeron.rcv.buffer.length=16384 -Dmultinode.Daeron.socket.so_sndbuf=2097152 -Dmultinode.Daeron.socket.so_rcvbuf=2097152 -Dmultinode.Daeron.rcv.initial.window.length=2097152 -Dmultinode.Dagrona.disable.bounds.checks=true -Dmultinode.XX:+UseCompressedOops -Dmultinode.XX:MaxDirectMemorySize=256m -Dmultinode.XX:+UnlockDiagnosticVMOptions -Dmultinode.XX:GuaranteedSafepointInterval=300000"
+declare -r MULTI_NODE_ARGS="-Dakka.test.multi-node=true -Dakka.test.multi-node.targetDirName=$MULTI_NODE_DIR -Dakka.remote.artery.advanced.flight-recorder.destination=target -J-Dmultinode.Xms1024M -J-Dmultinode.Xmx1024M -J-Dmultinode.XX:+PrintGCDetails -J-Dmultinode.XX:+PrintGCTimeStamps -Dmultinode.XX:BiasedLockingStartupDelay=0 -Dmultinode.Daeron.mtu.length=16384 -Dmultinode.Daeron.rcv.buffer.length=16384 -Dmultinode.Daeron.socket.so_sndbuf=2097152 -Dmultinode.Daeron.socket.so_rcvbuf=2097152 -Dmultinode.Daeron.rcv.initial.window.length=2097152 -Dmultinode.Dagrona.disable.bounds.checks=true -J-Dmultinode.XX:+UseCompressedOops -Dmultinode.XX:MaxDirectMemorySize=256m -J-Dmultinode.XX:+UnlockDiagnosticVMOptions -Dmultinode.XX:GuaranteedSafepointInterval=300000"
 declare -r COMMON_ARGS="-Dakka.test.LatencySpec.totalMessagesFactor=10 -Dakka.test.LatencySpec.repeatCount=3 -Dakka.test.MaxThroughputSpec.totalMessagesFactor=200 -Dakka.remote.artery.advanced.embedded-media-driver=off -Dakka.remote.artery.advanced.aeron-dir=$AERON_DIR"
 #declare -r COMMON_ARGS="-Dakka.remote.artery.advanced.embedded-media-driver=off -Dakka.remote.artery.advanced.aeron-dir=$AERON_DIR"
 
@@ -84,7 +92,7 @@ declare -r TEST="$2"
 echo "logs in $LOGS_BASE"
 mkdir -p $LOGS1
 
-# description of test 
+# description of test
 cd /home/akka/akka
 echo "$RGS" > $LOGS_BASE/readme.txt
 echo "MULTI_NODE_ARGS=$MULTI_NODE_ARGS" >> $LOGS_BASE/readme.txt
