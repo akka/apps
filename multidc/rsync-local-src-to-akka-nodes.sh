d7b1ebff7d43485a161342dac76a4937b80a08b5
Patrik Nordwall <patrik.nordwall@gmail.com>
2017-12-20 17:48:56 +0100

    nodes

--- multidc/rsync-local-src-to-akka-nodes.sh
+++ multidc/rsync-local-src-to-akka-nodes.sh
@@ -15,5 +15,5 @@ for node in "${nodes_all[@]}"; do
   ssh -i $HOME/.ssh/replicated-entity.pem akka@${node} "rm -rf multidc/apps/multidc/src"
   rsync -Pavuze "ssh -i $HOME/.ssh/replicated-entity.pem" src akka@${node}:multidc/apps/multidc/ &
 
-  scp -i $HOME/.ssh/replicated-entity.pem '/Users/ktoso/.ivy2/local/com.lightbend.akka/akka-persistence-multi-dc_2.11/1.1-M4+5-975f995e/jars/akka-persistence-multi-dc_2.11.jar' akka@${node}:/home/akka/akka-persistence-multi-dc_2.11.jar
+  scp -i $HOME/.ssh/replicated-entity.pem '~/.ivy2/local/com.lightbend.akka/akka-persistence-multi-dc_2.11/1.1-M5+21-30d17edd+20171214-1234/jars/akka-persistence-multi-dc_2.11.jar' akka@${node}:/home/akka/akka-persistence-multi-dc_2.11.jar
 done
